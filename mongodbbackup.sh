1db79947d3d59383c9fc3a5919420909409f9d40
Harikrishna TR <99418861+Harikrishnatr93@users.noreply.github.com>
2022-02-11 21:02:37 +0530

    Update mongodbbackup.sh

--- mongodbbackup.sh
+++ mongodbbackup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 
+#######################
+#Input MongoDB authentication details
+
 THEDB='mymongodb'
 THEDBPW='mongodb+srv://admin:jwnnwkncc@mymongodb.xyzc.mongodb.net'
 THEDATE=`date +%d_%m_%y_%H.%M`
@@ -8,6 +11,6 @@ THEDATE=`date +%d_%m_%y_%H.%M`
 # export database
 mongodump --uri="$THEDBPW" | gzip > /root/dbbackup/dbbackup_${THEDB}_${THEDATE}.gz
 
-
-# remove backups older than 3 days
+#######################
+# remove backups older than 3 days, You can change the 3 to any number as per your requirement
 find /root/dbbackup/dbbackup_* -mtime +3 -exec rm {} \;
